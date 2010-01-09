4f3bee6090706b192effd92d9a7c9c457735569d
GnunuX <gnunux@gnunux.info>
2010-01-09 23:43:01 +0100

    remove unicode warning

--- src-demo/api/tags.py
+++ src-demo/api/tags.py
@@ -2,10 +2,10 @@
 
 from bdd.tags import Tag, ConfLevel
 
-def add_tag(name, typ, description=''):
+def add_tag(name, typ, description=u''):
     return Tag(name=name, typ=typ, description=description)
 
-confuser = ConfLevel(typ='confuser', description="User's configuration")
-confcomputer = ConfLevel(typ='conflevel', description="Computer's configuration")
+confuser = ConfLevel(typ=u'confuser', description=u"User's configuration")
+confcomputer = ConfLevel(typ=u'conflevel', description=u"Computer's configuration")
 
 # vim: ts=4 sw=4 expandtab
