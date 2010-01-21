15c76d35af767e3d655c4d5d8f034defd6a8cc63
GnunuX <gnunux@gnunux.info>
2010-01-21 23:00:56 +0100

    corrections

---
+++ src-demo/api/categories.py
@@ -0,0 +1,32 @@
+# api's tags
+
+from bdd.tags import Tag, Category, ConfLevel
+
+# ____________________________________________________________
+# Wrapped objects
+ 
+class APICategory:
+    def __init__(self, wrap):
+        self._w = wrap
+      
+    def add_tag(self, tag):
+        self._w.tags.append(tag)
+#---
+
+def add_tag(name, comment=u''):
+    return Tag(name=name, comment=comment)
+
+def add_category(name, comment=u''):
+    ret = Category(name=name, comment=comment)
+    return APICategory(ret)
+
+def get_conflevel():
+    confuser = ConfLevel.query.filter_by(name=u'confuser').first()
+    if confuser == None:
+        confuser = ConfLevel(name=u'confuser', comment=u"User's configuration")
+    confcomputer = ConfLevel.query.filter_by(name=u'confcomputer').first()
+    if confcomputer == None:
+        confcomputer = ConfLevel(name=u'confcomputer', comment=u"Computer's configuration")
+    return (confuser, confcomputer)
+
+# vim: ts=4 sw=4 expandtab
---
+++ src-demo/bdd/categories.py
@@ -0,0 +1,26 @@
+from elixir import *
+
+class Tag(Entity):
+    name = Field(UnicodeText)
+    comment = Field(UnicodeText)
+    rules = OneToMany('Rule')
+    category = ManyToOne('Category')
+    def __repr__(self):
+        return 'name: "%s", comment: "%s"' % (self.name, self.comment)
+
+class Category(Entity):
+    name = Field(UnicodeText)
+    comment = Field(UnicodeText)
+    tags = OneToMany('Tag')
+
+    def __repr__(self):
+        return 'name: "%s", comment: "%s"' % (self.name, self.comment)
+
+class ConfLevel(Entity):
+    name = Field(UnicodeText)
+    comment = Field(UnicodeText)
+    rules = OneToMany('Rule')
+    def __repr__(self):
+        return 'name: "%s", comment: "%s"' % (self.name, self.comment)
+
+# vim: ts=4 sw=4 expandtab
